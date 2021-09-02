1ab39755878f1650a41bc2eff82521cf98225f24
Alok Maurya <alok@unacademy.com>
2021-09-03 01:05:45 +0530

    cluster issue fix

--- ratelimit/redis_rate_limit.py
+++ ratelimit/redis_rate_limit.py
@@ -52,7 +52,7 @@ class RedisRateLimiterConnection(object):
     def __init__(self, host=None, connection=None):
         self.connection = None
         if host and port:
-            connection = StrictRedisCluster(startup_nodes=[{'host': host, 'port': 6379}])
+            connection = StrictRedisCluster(startup_nodes=[{'host': host, 'port': 6379}], skip_full_coverage_check=True)
             if not connection.ping():
                 raise DatastoreConnectionError
             self.connection = connection
